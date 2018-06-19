80abe2f3abf6e56885d4488dd2efe39652195b94
Tyler Fisher <tfisher@politico.com>
2018-06-19 17:23:11 -0400

    0.4.0: townships

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='politico-civic-geography',
-    version="0.3.6",
+    version="0.4.0",
     description='',
     url='https://github.com/The-Politico/politico-civic-geography',
     author='POLITICO interactive news',
